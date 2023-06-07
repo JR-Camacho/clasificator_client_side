bc629295ecb1d648ff2a2dfa4e0041b384959bd9
JR-Camacho <josecamachoc0303@gmail.com>
2023-06-06 22:44:09 -0400

    The export problem of the home view is solved

--- src/Pages/Index.jsx
+++ src/Pages/Index.jsx
@@ -1,4 +1,4 @@
-import Home from "./Home/home";
+import Home from "./Home/Home";
 import Model from "./Model/Model";
 import Suport from "./Suport/Suport";
 import PageNotFound from "./NotFound/PageNotFound";
